42a627c59a090b4ec273e94e6fb8497a7a1c2612
wuyepabdul <wuyepabdul@gmail.com>
2021-09-23 12:00:59 +0100

    Add index base rendering of todo list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
 
 const toDoTasks = [
-  { index: 1, description: 'wash dishes', completed: false },
   { index: 2, description: 'complete To Do List Project', completed: false },
+  { index: 1, description: 'wash dishes', completed: false },
   { index: 3, description: 'Fix car', completed: false },
 ];
 
@@ -15,7 +15,7 @@ let todoMarkup = `
 <li class="todo-li">
   <input
     type="text"
-    class="todo-input"
+    class="todo-input add-todo"
     placeholder="Add to your list..."
   />
 </li>
@@ -26,16 +26,18 @@ const clearButton = "<li><button class='clear-btn'>Clear All completed</button><
 function component() {
   const element = document.createElement('div');
   element.classList.add('todo-div');
-  toDoTasks.forEach((task) => {
-    todoMarkup += `<li class="todo-li">
+  toDoTasks
+    .sort((object1, object2) => object1.index - object2.index)
+    .forEach((task) => {
+      todoMarkup += `<li class="todo-li">
     <div class="todo-input-div">
       <input type="checkbox" />
       <p class='todo-description'> ${task.description}</p>
     </div>
-     
     <p><i class="fas fa-ellipsis-v"></i></p>
   </li>`;
-  });
+    });
+
   element.innerHTML = todoMarkup + clearButton;
   return element;
 }
