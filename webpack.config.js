54d47790e3463849ecc780411d3fda12f5db2915
Wuyep Abdul <wuyepabdul@gmail.com>
2021-09-27 12:42:49 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   devtool: 'inline-source-map',
   entry: {
     index: './src/index.js',
