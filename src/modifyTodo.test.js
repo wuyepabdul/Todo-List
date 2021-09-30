2fddaa556ff5487665c289a929fe94ee3b6fb007
Niko Escobal <niko.escobal@gmail.com>
2021-09-30 23:30:56 +0800

    Add and Implement delete test

--- src/modifyTodo.test.js
+++ src/modifyTodo.test.js
@@ -1,10 +1,25 @@
-import { addTodo } from './__mocks__/modifyTodo.js';
+import {
+  addTodo,
+  deleteTodo
+} from './__mocks__/modifyTodo.js';
 
 jest.mock('./modifyTodo.js');
 
-const task1 = { index: 1, description: 'Wash dishes', completed: false };
-const task2 = { index: 2, description: 'Do laundry', completed: false };
-const task3 = { index: '', description: 'Go shopping', completed: false };
+const task1 = {
+  index: 1,
+  description: 'Wash dishes',
+  completed: false
+};
+const task2 = {
+  index: 2,
+  description: 'Do laundry',
+  completed: false
+};
+const task3 = {
+  index: 3,
+  description: 'Go shopping',
+  completed: false
+};
 
 const todoList = [task1, task2];
 
@@ -17,5 +32,16 @@ describe('Add a new task', () => {
     const updatedTaskList = addTodo(task3, todoList);
     const newTaskIndex = updatedTaskList[updatedTaskList.length - 1].index;
     expect(newTaskIndex).toStrictEqual(3);
+  });
+});
+
+describe('Delete a new task', () => {
+  test('should delete a task from the array', () => {
+    const arrayOfTasks = [task1, task2, task3];
+    expect(deleteTodo(task1.index, arrayOfTasks).length).toStrictEqual(2);
+  });
+  test('should check if array length correctly factors deleted item', () => {
+    const arrayOfTasks = [task1, task2, task3];
+    expect(deleteTodo(task1.index, arrayOfTasks).length).not.toStrictEqual(3);
   });
 });
