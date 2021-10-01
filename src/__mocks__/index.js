9edca27e6f8907e63674949b4aba3648f624d0b1
wuyepabdul <wuyepabdul@gmail.com>
2021-10-01 15:27:14 +0100

    Fix conflict

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -2,40 +2,6 @@ export const clearButton = () => {
   const btn = "<li><button type='button' class='clear-btn'>Clear All completed</button></li>";
   return btn;
 };
-<<<<<<< HEAD
-
-export const todoUlMarkup = () => {
-  const markup = `      
-  <ul class="todo-ul">
-  <li class="todo-li">
-    <h1>Today's To Do</h1>
-    <p><i class="fas fa-sync-alt"></i></p>
-  </li>
-  <li class="todo-li">
-    <input
-      type="text"
-      name="new-todo"
-      id="new-todo"
-      class="todo-input add-todo"
-      placeholder="Add to your list..."
-    />  
-    <button type='submit' class='add-btn'> Add</button>
-  </li>
-  </ul>`;
-  return markup;
-};
-
-export const todoLiMarkup = (task) => {
-  const markup = `<li class="todo-li todo-container">
-  <div class="todo-input-div">
-    <input type="checkbox" id="check" value=${task.index} checked />
-    <input name="edit-input" type="text" id="edit-input" class="todo-input isChecked" value="${task.description}" readonly/>
-  </div>
-  <p id="${task.index}"><i class="fas fa-ellipsis-v edit-todo "></i><i class="far fa-trash-alt delete-todo inActive"></i></p>
-</li>`;
-  return markup;
-};
-=======
 export const todoUlMarkup = () => {
   const markup = '        <ul class="todo-ul">  <li class="todo-li">    <h1>Today\'s To Do</h1>    <p><i class="fas fa-sync-alt"></i></p>  </li>  <li class="todo-li">    <input      type="text"      name="new-todo"      id="new-todo"      class="todo-input add-todo"      placeholder="Add to your list..."    />      <button type=\'submit\' class=\'add-btn\'> Add</button>  </li>  </ul>';
   return markup;
@@ -44,4 +10,3 @@ export const todoLiMarkup = (task) => {
   const markup = `<li class="todo-li todo-container">  <div class="todo-input-div">    <input type="checkbox" id="check" value=${task.index} checked />    <input name="edit-input" type="text" id="edit-input" class="todo-input isChecked" value="${task.description}" readonly/>  </div>  <p id="${task.index}"><i class="fas fa-ellipsis-v edit-todo "></i><i class="far fa-trash-alt delete-todo inActive"></i></p></li>`;
   return markup;
 };
->>>>>>> 9681832316843d8d99cb3f0011c9fedafbdda467
--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-/*
- * @jest-environment jsdom
- */
-
-import {
-  clearButton, todoLiMarkup, todoUlMarkup,
-} from '../__mocks__/index.js';
-import { addTodo } from '../__mocks__/modifyTodo.js';
-
-jest.mock('../index.js');
-
-=======
 /* * @jest-environment jsdom */
 import {
   clearButton,
@@ -22,33 +9,11 @@ import {
 } from '../__mocks__/modifyTodo.js';
 
 jest.mock('../index.js');
->>>>>>> 9681832316843d8d99cb3f0011c9fedafbdda467
 const task1 = {
   index: 1,
   description: 'Wash dishes',
   completed: false,
 };
-<<<<<<< HEAD
-const task2 = {
-  index: 2,
-  description: 'Do laundry',
-  completed: true,
-};
-const task3 = {
-  index: 3,
-  description: 'Go shopping',
-  completed: false,
-};
-
-const task4 = {
-  index: 4,
-  description: 'Go Driving',
-  completed: true,
-};
-
-const todoList = [task1, task2, task3];
-=======
->>>>>>> 9681832316843d8d99cb3f0011c9fedafbdda467
 
 describe('Dom manipulation', () => {
   test('Should add todo heading to dom', () => {
@@ -61,26 +26,14 @@ describe('Dom manipulation', () => {
     expect(headingTag).toBe("Today's To Do");
     expect(clearBtn).toBeTruthy();
   });
-<<<<<<< HEAD
-
-=======
->>>>>>> 9681832316843d8d99cb3f0011c9fedafbdda467
   test('Should add created todo to dom', () => {
     const divElement = document.createElement('div') + todoUlMarkup();
     const arrayOfTasks = [];
     const newTodo = addTodo(task1, arrayOfTasks);
     document.body.innerHTML = divElement + todoLiMarkup(task1) + clearButton();
     const todosInDom = document.querySelectorAll('.todo-container');
-<<<<<<< HEAD
 
     expect(newTodo.length).toStrictEqual(1);
     expect(todosInDom.length).toBe(1);
   });
-
 });
-=======
-    expect(newTodo.length).toStrictEqual(1);
-    expect(todosInDom.length).toBe(1);
-  });
-});
->>>>>>> 9681832316843d8d99cb3f0011c9fedafbdda467
